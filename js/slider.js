b00c53da90ba6e2986876f207c3b0789d237a803
Ahmadsabili <ahmadsabili0081@gmail.com>
2021-11-02 00:58:02 +0700

    initial commit

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,26 @@
- var swiper = new Swiper(".mySwiper", {
-     slidesPerView: 3,
-     loop: true,
-     spaceBetween: 350,
-      autoplay: { 
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView: 1,
-        },
-        550: {
-            slidesPerView: 2,
-        },
-        800:{
-            slidesPerView: 3,
-        },
-        980:{
-            slidesPerView: 3,
-        },
-        1000:{
-            slidesPerView: 4,
-        },
-    },
-        scrollbar: {
-          el: ".swiper-scrollbar",
-          snapOnRelease: true,
-          draggable: true,
-          lockClass: 'swiper-scrollbar-lock',
-        },
+const slider = document.querySelector('.items');
+let isDown = false;
+let slideX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+    isDown = true;
+    slider.classList.add('active');
+    slideX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - slideX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
 });
