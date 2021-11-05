a42fed7e6a06cfe7852967e575a65017e1e4e62d
Ahmadsabili <ahmadsabili0081@gmail.com>
2021-11-05 17:53:43 +0700

    initial commit

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,24 @@
+
+window.addEventListener('scroll', function() {
+  let btnScroll = document.querySelector('.btn');
+  btnScroll.classList.toggle('showBtn', window.scrollY > 50);
+});
+function btnScrolll(){
+  document.body.scrollTop = 0;
+
+  document.documentElement.scrollTop = 0;
+}
+
+function myLoader(){
+  var loader = setTimeout(showpage,3000);
+}
+function showpage(){
+  document.getElementById('loader').style.display="none";
+  document.getElementById('hide').style.visibility="unset";
+}
 AOS.init({
     disable: false,
     offset: 120,
     duration: 400,
   });
+
